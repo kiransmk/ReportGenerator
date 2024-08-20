db5d5558cffcde3d7a74fea40034f14f84cbe6e8
Kiran Kumar <kiransmk@gmail.com>
2024-08-19 17:10:20 -0700

    update code to make manual run

--- generate_html.py
+++ generate_html.py
@@ -4,6 +4,7 @@ import sys
 import re
 from collections import defaultdict
 import mimetypes
+from datetime import datetime
 
 # Define MIME type to language mapping
 mime_to_language = {
@@ -51,24 +52,29 @@ def group_by_flagKey(data):
             grouped_data[row['flagKey']].append(row)
     return grouped_data
 
-def generate_html(data, output_html):
+def generate_html(data, output_html, repoName, branchName):
     # Load the Jinja2 template
     env = Environment(loader=FileSystemLoader('.'))
     template = env.get_template('template.html')
 
+    # Create a timestamp
+    timestamp = datetime.now().strftime("%B %d, %Y at %I:%M %p")
+
     # Render the template with the data
-    html_content = template.render(data=data)
+    html_content = template.render(data=data, timestamp=timestamp, repoName=repoName, branchName=branchName)
 
     # Write the HTML content to a file
     with open(output_html, 'w') as file:
         file.write(html_content)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python generate_html.py <input_csv> <output_html>")
+    if len(sys.argv) != 5:
+        print("Usage: python generate_html.py <input_csv> <output_html> repoName branchName")
     else:
         input_csv = sys.argv[1]
         output_html = sys.argv[2]
+        repoName = sys.argv[3]
+        branchName = sys.argv[4]
         
         # Read CSV data into an object
         data = read_csv_to_object(input_csv)
@@ -80,4 +86,4 @@ if __name__ == "__main__":
         grouped_data = group_by_flagKey(data)
 
         # Generate HTML using the data
-        generate_html(grouped_data, output_html)
+        generate_html(grouped_data, output_html, repoName, branchName)
